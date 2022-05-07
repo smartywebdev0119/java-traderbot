879221ea40388a8d17546e4ba5f864743dc3a665
smartywebdev0119 <smartwebdev0119@gmail.com>
2022-05-07 22:17:21 +0200

    First file published

--- src/main/java/com/tecknobit/traderbot/Traders/BinanceTraderBot.java
+++ src/main/java/com/tecknobit/traderbot/Traders/BinanceTraderBot.java
@@ -2,16 +2,20 @@ package com.tecknobit.traderbot.Traders;
 
 import com.tecknobit.binancemanager.Exceptions.SystemException;
 import com.tecknobit.binancemanager.Managers.Market.BinanceMarketManager;
+import com.tecknobit.binancemanager.Managers.Market.Records.Tickers.TickerPriceChange;
 import com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.BinanceSpotManager;
+import com.tecknobit.binancemanager.Managers.SignedManagers.Trade.Spot.Records.Account.SpotAccountInformation;
 import com.tecknobit.traderbot.Routines.TradersCoreRoutines;
 
 import java.io.IOException;
+import java.util.HashMap;
+
+import static com.tecknobit.binancemanager.Managers.SignedManagers.Wallet.Records.AccountSnapshots.AccountSnapshotSpot.BalancesSpot;
 
 public class BinanceTraderBot extends TradersCoreRoutines {
 
-    public static final String COMPARE_CURRENCY = "BNB";
+    public static final String COMPARE_CURRENCY = "USDT";
     private final BinanceSpotManager binanceSpotManager;
-
     private final BinanceMarketManager binanceMarketManager;
 
     public BinanceTraderBot(String apiKey, String secretKey) throws SystemException, IOException {
@@ -24,12 +28,33 @@ public class BinanceTraderBot extends TradersCoreRoutines {
         binanceMarketManager = new BinanceMarketManager();
     }
 
-    public double getWalletBalance() throws Exception {
-        return 0;
+    public double getWalletBalance(String currency) throws Exception {
+        SpotAccountInformation accountInformation = binanceSpotManager.getObjectSpotAccountInformation();
+        HashMap<String, Double> lastPrice = new HashMap<>();
+        double balance = 0;
+        for(TickerPriceChange tickerPriceChange : binanceMarketManager.getTickerPriceChangeList()) {
+            String symbol = tickerPriceChange.getSymbol();
+            if(symbol.endsWith(COMPARE_CURRENCY))
+                lastPrice.put(symbol, tickerPriceChange.getLastPrice());
+        }
+        for (BalancesSpot balancesSpot : accountInformation.getBalancesSpots()) {
+            double free = balancesSpot.getFree();
+            if(free > 0) {
+                try {
+                    balance += free * lastPrice.get(balancesSpot.getAsset() + COMPARE_CURRENCY);
+                }catch (Exception ignored){}
+            }
+        }
+        if(!currency.equals(COMPARE_CURRENCY)){
+            try {
+                balance /= binanceMarketManager.getCurrentAveragePriceValue(currency + COMPARE_CURRENCY);
+            }catch (Exception ignored){}
+        }
+        return balance;
     }
 
-    public double getWalletBalance(int decimals) throws Exception {
-        return binanceMarketManager.roundValue(getWalletBalance(), decimals);
+    public double getWalletBalance(String currency, int decimals) throws Exception {
+        return binanceMarketManager.roundValue(getWalletBalance(currency), decimals);
     }
 
     @Override
