17ec27db97ee23a33f653c97e147d8cdf5ed4b42
smartywebdev0119 <smartwebdev0119@gmail.com>
2022-05-07 15:00:01 +0200

    First file published

--- src/main/java/com/tecknobit/traderbot/Routines/TradersCoreRoutines.java
+++ src/main/java/com/tecknobit/traderbot/Routines/TradersCoreRoutines.java
@@ -6,10 +6,12 @@ public abstract class TradersCoreRoutines {
 
     public abstract double getWalletBalance();
 
-    public abstract <T> ArrayList<T> getAssetsList();
+    public abstract <T> ArrayList<T> getAssetsList() throws Exception;
 
     public abstract <T> ArrayList<T> getTransactionsList();
 
     public abstract <T> T getAccountDetails();
+
+    public abstract String getErrorResponse();
 
 }
--- src/main/java/com/tecknobit/traderbot/Traders/BinanceTraderBot.java
+++ src/main/java/com/tecknobit/traderbot/Traders/BinanceTraderBot.java
@@ -2,19 +2,45 @@ package com.tecknobit.traderbot.Traders;
 
 import com.tecknobit.binancemanager.Exceptions.SystemException;
 import com.tecknobit.binancemanager.Managers.SignedManagers.Wallet.BinanceWalletManager;
+import com.tecknobit.traderbot.Routines.TradersCoreRoutines;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class BinanceTraderBot {
+public class BinanceTraderBot extends TradersCoreRoutines {
 
     private final BinanceWalletManager binanceWalletManager;
 
     public BinanceTraderBot(String apiKey, String secretKey) throws SystemException, IOException {
-        this.binanceWalletManager = new BinanceWalletManager(apiKey, secretKey);
+        binanceWalletManager = new BinanceWalletManager(apiKey, secretKey);
     }
 
     public BinanceTraderBot(String apiKey, String secretKey, String baseEndpoint) throws SystemException, IOException {
-        this.binanceWalletManager = new BinanceWalletManager(apiKey, secretKey, baseEndpoint);
+        binanceWalletManager = new BinanceWalletManager(apiKey, secretKey, baseEndpoint);
     }
 
+    @Override
+    public double getWalletBalance() {
+        return 0;
+    }
+
+    @Override
+    public <T> ArrayList<T> getAssetsList() throws Exception {
+        return null;
+    }
+
+    @Override
+    public <T> ArrayList<T> getTransactionsList() {
+        return null;
+    }
+
+    @Override
+    public <T> T getAccountDetails() {
+        return null;
+    }
+
+    @Override
+    public String getErrorResponse() {
+        return binanceWalletManager.getErrorResponse();
+    }
 }
