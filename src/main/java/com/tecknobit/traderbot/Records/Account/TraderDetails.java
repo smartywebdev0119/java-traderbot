abbcacc1b85ad77d0d02f76f600825146e3ed481
smartywebdev0119 <smartwebdev0119@gmail.com>
2022-06-05 20:27:48 +0200

    First file published

--- src/main/java/com/tecknobit/traderbot/Records/Account/TraderDetails.java
+++ src/main/java/com/tecknobit/traderbot/Records/Account/TraderDetails.java
@@ -1,8 +1,12 @@
 package com.tecknobit.traderbot.Records.Account;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import static java.text.DateFormat.DATE_FIELD;
+import static java.text.DateFormat.getDateInstance;
 
 /**
  * The {@code TraderDetails} class is useful to contains details of trader used. <br>
@@ -13,9 +17,15 @@ import java.util.Locale;
 public final class TraderDetails {
 
     /**
-     * {@code format} is instance that memorize format of {@link #lastTraderActivity} timestamp
+     * {@code dayPassFormat} is instance that memorize format of {@link #lastTraderActivity} when a day from last activity
+     * is passed
      * **/
-    private static final DateFormat format =  DateFormat.getDateInstance(DateFormat.MINUTE_FIELD, Locale.getDefault());
+    private static final DateFormat dayPassFormat = getDateInstance(DATE_FIELD, Locale.getDefault());
+
+    /**
+     * {@code timeFormat} is instance that memorize default format of {@link #lastTraderActivity}
+     * **/
+    private static final SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
 
     /**
      * {@code TRADER_TYPE_AUTONOMOUS} is instance that memorize AUTONOMOUS trader type in use
@@ -51,6 +61,11 @@ public final class TraderDetails {
      * {@code lastTraderActivity} is instance that memorize last time when bot contact server
      * **/
     private String lastTraderActivity;
+
+    /**
+     * {@code lastTraderActivityTimestamp} is instance that memorize last time when bot contact server in timestamp format
+     * **/
+    private long lastTraderActivityTimestamp;
 
     /**
      * {@code traderType} is instance that memorize type of trader in use {@link #TRADER_TYPE_AUTONOMOUS} or {@link #TRADER_TYPE_MANUAL}
@@ -107,48 +122,14 @@ public final class TraderDetails {
      * @param refreshPricesTime: time for refresh last prices
      * @param runningFromDate: date when trader has been started
      * **/
-    public TraderDetails(String lastTraderActivity, String traderType, String traderStatus, String traderPlatform,
-                         int refreshPricesTime, String runningFromDate) {
-        if(lastTraderActivity == null || lastTraderActivity.trim().isEmpty())
-            throw new IllegalArgumentException("Last trader activity cannot be null or empty");
-        else
-            this.lastTraderActivity = lastTraderActivity;
-        if(!traderType.equals(TRADER_TYPE_AUTONOMOUS) && !traderType.equals(TRADER_TYPE_MANUAL))
-            throw new IllegalArgumentException("Trader type inserted is wrong value, can be AUTONOMOUS or MANUAL type");
-        else
-            this.traderType = traderType;
-        if(!traderStatus.equals(RUNNING_TRADER_STATUS) && !traderStatus.equals(STOPPED_TRADER_STATUS))
-            throw new IllegalArgumentException("Trader status inserted is wrong value, can be RUNNING or STOPPED status");
-        else
-            this.traderStatus = traderStatus;
-        if(!traderPlatform.equals(BINANCE_TRADER_PLATFORM) && !traderPlatform.equals(COINBASE_TRADER_PLATFORM))
-            throw new IllegalArgumentException("Trader platform inserted is not supported yet or is a wrong value");
-        else
-            this.traderPlatform = traderPlatform;
-        if(refreshPricesTime < 5 || refreshPricesTime > 3600)
-            throw new IllegalArgumentException("Refresh prices time must be more than 5 (5s) and less than 3600 (1h)");
-        else
-            this.refreshPricesTime = refreshPricesTime;
-        if(runningFromDate == null || runningFromDate.trim().isEmpty())
-            throw new IllegalArgumentException("Running from date cannot be null or empty");
-        else
-            this.runningFromDate = runningFromDate;
-    }
-
-    /** Constructor to init {@link TraderDetails}
-     * @param lastTraderActivity: last time when bot contact server
-     * @param traderType: type of trader in use {@link #TRADER_TYPE_AUTONOMOUS} or {@link #TRADER_TYPE_MANUAL}
-     * @param traderStatus: current status of trader {@link #RUNNING_TRADER_STATUS} or {@link #STOPPED_TRADER_STATUS}
-     * @param traderPlatform: platform of trader in use {@link #BINANCE_TRADER_PLATFORM} or {@link #COINBASE_TRADER_PLATFORM}
-     * @param refreshPricesTime: time for refresh last prices
-     * @param runningFromDate: date when trader has been started
-     * **/
     public TraderDetails(long lastTraderActivity, String traderType, String traderStatus, String traderPlatform,
                          int refreshPricesTime, String runningFromDate) {
         if(lastTraderActivity < 0)
             throw new IllegalArgumentException("Last trader activity timestamp cannot be less than 0");
-        else
-            this.lastTraderActivity = format.format(new Date(lastTraderActivity));
+        else {
+            this.lastTraderActivity = timeFormat.format(new Date(lastTraderActivity));
+            lastTraderActivityTimestamp = lastTraderActivity;
+        }
         if(!traderType.equals(TRADER_TYPE_AUTONOMOUS) && !traderType.equals(TRADER_TYPE_MANUAL))
             throw new IllegalArgumentException("Trader type inserted is wrong value, can be AUTONOMOUS or MANUAL type");
         else
@@ -172,19 +153,16 @@ public final class TraderDetails {
     }
 
     public String getLastTraderActivity() {
+        if((System.currentTimeMillis() - lastTraderActivityTimestamp) >= ((86400 * 1000) / 2))
+            lastTraderActivity = dayPassFormat.format(new Date(lastTraderActivityTimestamp));
         return lastTraderActivity;
-    }
-
-    public void setLastTraderActivity(String lastTraderActivity) {
-        if(lastTraderActivity == null || lastTraderActivity.trim().isEmpty())
-            throw new IllegalArgumentException("Last trader activity cannot be null or empty");
-        this.lastTraderActivity = lastTraderActivity;
     }
 
     public void setLastTraderActivity(long lastTraderActivity) {
         if(lastTraderActivity < 0)
             throw new IllegalArgumentException("Last trader activity timestamp cannot be less than 0");
-        this.lastTraderActivity = format.format(new Date(lastTraderActivity));
+        this.lastTraderActivity = timeFormat.format(new Date(lastTraderActivity));
+        lastTraderActivityTimestamp = lastTraderActivity;
     }
 
     public String getTraderType() {
@@ -218,12 +196,6 @@ public final class TraderDetails {
 
     public String getRunningFromDate() {
         return runningFromDate;
-    }
-
-    public static void main(String[] args) {
-        TraderDetails traderDetails = new TraderDetails(System.currentTimeMillis(), TRADER_TYPE_AUTONOMOUS,
-                STOPPED_TRADER_STATUS, COINBASE_TRADER_PLATFORM, 5, "05/06/2022");
-        System.out.println(traderDetails.getLastTraderActivity());
     }
 
 }
